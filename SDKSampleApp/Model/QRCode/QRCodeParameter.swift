329012e5fb4f403180ca4cc9cad1c82da60452fa
Abdelaziz Mezri <amezri@fuzyo.com>
2024-08-06 15:24:15 +0100

    remove login, environment and qrcode files

--- SDKSampleApp/Model/QRCode/QRCodeParameter.swift
+++
@@ -1,7 +0,0 @@
-enum QRCodeParameter: String {
-    case env
-    case cu
-    case bu
-    case source
-    case sessionToken
-}
--- SDKSampleApp/ViewControllers/MainNavigationController.swift
+++ SDKSampleApp/ViewControllers/MainNavigationController.swift
@@ -13,15 +13,11 @@ import iOSClientExposurePlayback
 /// Handles the main navigation in the app
 class MainNavigationController: UINavigationController {
     
-    var qrCodeData: QRCodeData?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         if StorageProvider.storedSessionToken != nil {
             viewControllers = [RootViewController()]
         } else {
-            
-            //viewControllers = [EnvironmentViewController()]
             
             //*
             let environment = Environment(baseUrl: "https://exposure.api.redbee.live", customer: "TV5MONDE", businessUnit: "TV5MONDEplus")
@@ -47,57 +43,14 @@ class MainNavigationController: UINavigationController {
                         StorageProvider.store(environment: environment)
                         StorageProvider.store(sessionToken: credentials.sessionToken)
                         
-                        reloadAppNavigation()
+                        
                     }
             }
             //*/
         }
-        tryPlayingAssetIfPossible()
     }
     
     override var preferredStatusBarStyle : UIStatusBarStyle {
         return .lightContent
-    }
-}
-
-// MARK: - QR Code related funcs
-
-extension MainNavigationController {
-    
-    func tryPlayingAssetIfPossible() {
-        if let qrCodeData, qrCodeData.isContentAssetAvailable {
-            showPlayerController(
-                qrCodeData: qrCodeData,
-                environment: StorageProvider.storedEnvironment
-            )
-        }
-    }
-    
-    private func showPlayerController(
-        qrCodeData: QRCodeData,
-        environment: Environment?
-    ) {
-        guard let source = qrCodeData.urlParams?.source else {
-            return
-        }
-        
-        let playerVC = PlayerViewController()
-        
-        if qrCodeData.isSourceAssetURL,
-           let sourceURL = URL(string: source) {
-            /// assetURL
-            playerVC.shouldPlayWithUrl = true
-            playerVC.urlPlayable = URLPlayable(url: sourceURL)
-            viewControllers.append(playerVC)
-        } else if let sessionToken = StorageProvider.storedSessionToken,
-                  let environment = StorageProvider.storedEnvironment {
-            /// assetID
-            playerVC.sessionToken = sessionToken
-            playerVC.environment = environment
-            playerVC.shouldPlayWithUrl = false
-            playerVC.playable = AssetPlayable(assetId: source)
-            viewControllers.append(playerVC)
-        }
-        
     }
 }
